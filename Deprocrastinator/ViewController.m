f2f4533564324ca69662f62ca4084f6845e7f6ae
Matt Larkin <mattlarkin@me.com>
2015-03-17 00:17:01 -0400

    First Commit

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) IBOutlet UITextField *taskTextField;
+@property (strong, nonatomic) IBOutlet UITableView *taskTableView;
+@property BOOL isEditing;
+@property BOOL deletingByClickingOnDeleteButton;
+@property NSIndexPath *indexPathToDelete;
+@property NSMutableArray *taskArray;
 
 @end
 
@@ -16,12 +23,119 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    //Preload objects into Array
+    self.taskArray = [NSMutableArray new];
+
+    //Disable editing
+    self.editing = false;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// Method: OnAddButtonPressed
+// Action: Takes data from text field, passes it into array and reloads data.
+-(IBAction)onAddButtonPressed:(id)sender
+{
+    if ([self.taskTextField.text length] > 0) {
+        [self.taskArray addObject:self.taskTextField.text];
+        [self.taskTableView reloadData];
+         self.taskTextField.text = nil;
+        [self.taskTextField resignFirstResponder];
 }
+
+
+}
+- (IBAction)onEditButtonPressed:(UIButton *)sender {
+
+    // toggle text
+    if (!self.isEditing) {
+        [sender setTitle:@"Done" forState:UIControlStateNormal];
+        [self.taskTableView setEditing:YES animated:YES];
+    }
+    else {
+        [sender setTitle:@"Edit" forState:UIControlStateNormal];
+        [self.taskTableView setEditing:NO animated:YES];
+    }
+
+    self.isEditing = !self.isEditing;
+}
+
+//Set editing on Cell
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [self.taskArray removeObjectAtIndex:indexPath.row];
+        [tableView reloadData];
+}
+}
+
+
+#pragma marks - UITableView Datasource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.taskArray count];
+
+}
+
+//Allows deletion of data from table
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    return YES;
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ReminderCell"];
+    cell.textLabel.text = [self.taskArray objectAtIndex:indexPath.row];
+
+    return cell;
+}
+
+
+
+
+# pragma mark - Mark task as complete
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+
+    cell.textLabel.textColor = [UIColor greenColor];
+
+
+}
+
+
+
+
+
+
+# pragma mark - Gesture Handler
+
+- (IBAction)onSwipe:(UISwipeGestureRecognizer *)gestureRecognizer
+{
+    // find the cell that was tapped
+    if (gestureRecognizer.state == UIGestureRecognizerStateEnded) {
+        CGPoint location = [gestureRecognizer locationInView:self.taskTableView];
+        NSIndexPath *indexPath = [self.taskTableView indexPathForRowAtPoint:location];
+        UITableViewCell *cell = [self.taskTableView cellForRowAtIndexPath:indexPath];
+
+        // if the cell exists and it has text, change its text color
+        if (cell && [cell.textLabel.text length] > 0) {
+            [self setCellTextLabelPriorityColor:cell];
+
+            // save the color in the array
+            NSIndexPath *cellIndexPath = [self.tableView indexPathForCell:cell];
+            [self.todoListBackgroundColors setObject:cell.backgroundColor atIndexedSubscript:cellIndexPath.row];
+
+        }
+    }
+}
+
+
+
 
 @end
